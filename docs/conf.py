b96fc3a9697e0a1609810b3f3e20569b532e6037
peteGSX <peteracole@outlook.com.au>
2022-06-01 12:38:53 +1000

    Add them options

--- docs/conf.py
+++ docs/conf.py
@@ -50,3 +50,13 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'style_nav_header_background': 'white',
+    'logo_only': True,
+    # Toc options
+    'includehidden': True,
+    'titles_only': True,
+    'collapse_navigation': False,
+    'navigation_depth': -1 
+}
